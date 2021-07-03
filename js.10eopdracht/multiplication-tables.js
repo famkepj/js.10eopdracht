cfa868f660163054f1ce177574945f6d147919b5
famkepj <82056762+famkepj@users.noreply.github.com>
2021-07-03 18:44:25 +0200

    Add files via upload

---
+++ js.10eopdracht/multiplication-tables.js
@@ -0,0 +1,12 @@
+for (i = 1; i <= 10; i++) {
+    const uitkomst = i * 9;
+    console.log (i + " x 9 = " + (uitkomst));
+}
+
+// for (i = 1; i <= 10; i++) {
+//     for (a = 1; a <= 10; a++){
+//     }
+//     const uitkomst = i * a;
+//     console.log (i + " x "+ a +"=" + (uitkomst));
+// }
+
